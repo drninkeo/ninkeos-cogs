0320774e0ace20c06e912d1aeee345b26d59ab9e
Dr Ninkeo <14227565+drninkeo@users.noreply.github.com>
2020-02-15 19:32:04 +1100

    Update muteuser.py

--- muteuser/muteuser.py
+++ muteuser/muteuser.py
@@ -4,7 +4,9 @@ class muteuser(commands.Cog):
     """Mute a user"""
 
     @commands.command()
-    async def mycom(self, ctx):
+    async def mute(self, ctx, user: discord.Member = None):
         """This does stuff!"""
         # Your code will go here
+        if user is None:
+            await ctx.send("Error: you need to define a user to mute.")
         await ctx.send("I can do stuff!")
