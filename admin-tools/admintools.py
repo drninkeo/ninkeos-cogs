62c04b97bcd8ba2d8707fe4ffd0b4c91c5ae4b01
Dr Ninkeo <14227565+drninkeo@users.noreply.github.com>
2020-07-10 21:28:19 +1000

    Update admintools.py

--- admin-tools/admintools.py
+++ admin-tools/admintools.py
@@ -5,10 +5,9 @@ class admintools(commands.Cog):
     """Do admin things, like managing your channels, members and the like."""
 
     @commands.command()
-    async def mute(self, ctx, user: discord.Member = None):
-        """This does stuff!"""
+    async def muterole(self, ctx, user: discord.Member = None):
+        """Mutes a user by giving them a specific mute role instead of applying per channel permissions. This respects server hiearchy."""
         # Your code will go here
         if user is None:
             await ctx.send("Error: you need to define a user to mute.")
-            
         await ctx.send("I can do stuff!")
